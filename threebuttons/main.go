6719052eb25d9b31d592e016fa3b3c8b7726453c
rj <rj@localhost>
2017-11-24 12:39:48 -0500

    Added a new example program, which just has three buttons.  This example is similar to onebutton, in that it only exercises a limited range of widgets, but it expands so that layout options can be tested.

---
+++ threebuttons/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"goey"
+	"strconv"
+)
+
+var (
+	mainWindow *goey.MainWindow
+	clickCount int
+)
+
+func main() {
+	mw, err := goey.NewMainWindow("One Button", render())
+	if err != nil {
+		println(err.Error())
+		return
+	}
+	defer mw.Close()
+	mw.SetAlignment(goey.SpaceAround, goey.Stretch)
+	mainWindow = mw
+
+	goey.Run()
+}
+
+func update() {
+	err := mainWindow.SetChildren(render())
+	if err != nil {
+		fmt.Println("Error: ", err.Error())
+	}
+}
+
+func onclick(ndx int) func() {
+	return func() {
+		fmt.Println("click", ndx)
+	}
+}
+
+func onfocus(ndx int) func() {
+	return func() {
+		fmt.Println("focus", ndx)
+	}
+}
+
+func onblur(ndx int) func() {
+	return func() {
+		fmt.Println("blur", ndx)
+	}
+}
+
+func render() []goey.Widget {
+	text := "Click me!"
+	if clickCount > 0 {
+		text = text + "  (" + strconv.Itoa(clickCount) + ")"
+	}
+	return []goey.Widget{
+		&goey.Button{Text: text,
+			OnClick: func() {
+				clickCount++
+				update()
+			},
+			OnFocus: onfocus(1),
+			OnBlur:  onblur(1),
+		},
+		&goey.Button{Text: "Extra button",
+			OnClick: onclick(2),
+			OnFocus: onfocus(2),
+			OnBlur:  onblur(2),
+		},
+		&goey.Button{Text: "Supplefluous button",
+			OnClick: onclick(3),
+			OnFocus: onfocus(3),
+			OnBlur:  onblur(3),
+		},
+	}
+}
