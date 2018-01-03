f78e58fb42dbf507d28d32a8eb99653be8a966ca
rj <rj@localhost>
2018-01-03 15:13:12 -0500

    Fixed a bug in the event handling for TextArea to prevent infinite loops during 'changed' event.

--- textarea_linux.go
+++ textarea_linux.go
@@ -3,15 +3,17 @@ package goey
 import (
 	"unsafe"
 
+	"github.com/gotk3/gotk3/glib"
 	"github.com/gotk3/gotk3/gtk"
 )
 
 type mountedTextArea struct {
-	NativeWidget
+	handle *gtk.TextView
 
 	onChange func(string)
-	onFocus  func()
-	onBlur   func()
+	shChange glib.SignalHandle
+	onFocus  focusSlot
+	onBlur   blurSlot
 }
 
 func (w *TextArea) mount(parent NativeWidget) (MountedWidget, error) {
@@ -26,38 +28,31 @@ func (w *TextArea) mount(parent NativeWidget) (MountedWidget, error) {
 		buffer.Unref()
 		return nil, err
 	}
+	control.SetWrapMode(gtk.WRAP_WORD)
 	buffer.Unref()
-
-	//frame, err := gtk.FrameNew("")
-	//if err != nil {
-	//	control.Destroy()
-	//	return nil, err
-	//}
-	//frame.Add(control)
 	(*gtk.Container)(unsafe.Pointer(parent.handle)).Add(control)
 
 	retval := &mountedTextArea{
-		NativeWidget: NativeWidget{&control.Widget},
-		onChange:     w.OnChange,
-		onFocus:      w.OnFocus,
-		onBlur:       w.OnBlur,
+		handle:   control,
+		onChange: w.OnChange,
 	}
 
-	if w.OnChange != nil {
-		control.Connect("changed", textarea_onChanged, retval)
-	}
 	control.Connect("destroy", textarea_onDestroy, retval)
+	if w.OnChange != nil {
+		sh, err := buffer.Connect("changed", textarea_onChanged, retval)
+		if err != nil {
+			panic("Failed to connect 'changed' event")
+		}
+		retval.shChange = sh
+	}
+	retval.onFocus.Set(&control.Widget, w.OnFocus)
+	retval.onBlur.Set(&control.Widget, w.OnBlur)
 	control.Show()
 
 	return retval, nil
 }
 
-func textarea_onChanged(widget *gtk.TextView, mounted *mountedTextArea) {
-	buffer, err := widget.GetBuffer()
-	if err != nil {
-		// TODO:  What is the correct reporting here
-		return
-	}
+func textarea_onChanged(buffer *gtk.TextBuffer, mounted *mountedTextArea) {
 	text, err := buffer.GetText(buffer.GetStartIter(), buffer.GetEndIter(), true)
 	if err != nil {
 		// TODO:  What is the correct reporting here
@@ -70,6 +65,46 @@ func textarea_onDestroy(widget *gtk.TextView, mounted *mountedTextArea) {
 	mounted.handle = nil
 }
 
+func (w *mountedTextArea) Close() {
+	if w.handle != nil {
+		w.handle.Destroy()
+		w.handle = nil
+	}
+}
+
+func (w *mountedTextArea) Handle() *gtk.Widget {
+	return &w.handle.Widget
+}
+
 func (w *mountedTextArea) updateProps(data *TextArea) error {
-	panic("not implemented")
+	// TextView will send a 'changed' event, even if the new value is the
+	// same.  To stop an infinite loop, we need to protect by checking
+	// ourselves.
+	buffer, err := w.handle.GetBuffer()
+	if err != nil {
+		return err
+	}
+	oldText, err := buffer.GetText(buffer.GetStartIter(), buffer.GetEndIter(), true)
+	if err != nil {
+		return err
+	}
+	if data.Value != oldText {
+		buffer.SetText(data.Value)
+	}
+
+	w.onChange = data.OnChange
+	if data.OnChange != nil && w.shChange == 0 {
+		sh, err := buffer.Connect("changed", textinput_onChanged, w)
+		if err != nil {
+			panic("Failed to connect 'changed' event")
+		}
+		w.shChange = sh
+	} else if data.OnChange == nil && w.shChange != 0 {
+		buffer.HandlerDisconnect(w.shChange)
+		w.shChange = 0
+	}
+	w.onFocus.Set(&w.handle.Widget, data.OnFocus)
+	w.onBlur.Set(&w.handle.Widget, data.OnBlur)
+
+	return nil
 }
