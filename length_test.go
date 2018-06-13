7300f6052ba51f391814f3eb509ab5c08010616c
rj <rj@localhost>
2018-06-13 12:53:59 -0400

    Added some examples to document the use of Length.

--- length_test.go
+++ length_test.go
@@ -1,8 +1,31 @@
 package goey
 
 import (
+	"fmt"
 	"testing"
 )
+
+func ExampleLength() {
+	// Since there are 96 device-independent pixels per inch, and 72 points
+	// per inch, the following two lengths should be equal.
+	length1 := 96 * DIP
+	length2 := 72 * DIP
+
+	if length1 == length2 {
+		fmt.Printf("All is OK with the world.")
+	} else {
+		fmt.Printf("This should not happen, unless there is a bad rounding error.")
+	}
+}
+
+func ExampleLength_String() {
+	fmt.Printf("Converting:  1pt is equal to %sdip\n", 1*PT)
+	fmt.Printf("Converting:  1pt is equal to %1.2fdip\n", (1 * PT).DIP())
+
+	// Output:
+	// Converting:  1pt is equal to 1:21dip
+	// Converting:  1pt is equal to 1.33dip
+}
 
 func TestLength(t *testing.T) {
 	t.Logf("Constants DIP: %d", 1*DIP)
