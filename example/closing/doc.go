319e09eedbf1ed19f8b481d4caa220b056e3c9ec
rj <rj@localhost>
2018-07-10 16:22:32 -0400

    Doc comment for package.

---
+++ example/closing/doc.go
@@ -0,0 +1,5 @@
+// This package provides an example application built using the goey package.
+// The purpose of this package is to demontrate using the OnClosing callback for
+// windows.  Trying to close the window using the normal method will fail, but
+// the button within the window can be used.
+package main
