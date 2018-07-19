3dbe6fe3943967aa12eb9c8af8a61066432f2eae
rj <rj@localhost>
2018-07-19 12:51:29 -0400

    Additional testing for the type Size.

--- base/size_test.go
+++ base/size_test.go
@@ -3,6 +3,7 @@ package base
 import (
 	"fmt"
 	"image"
+	"testing"
 )
 
 func ExampleSize_FromPixels() {
@@ -15,4 +16,25 @@ func ExampleSize_FromPixels() {
 
 	// Output:
 	// The size is (48:00x192:00).
+}
+
+func TestSize(t *testing.T) {
+	cases := []struct {
+		in     Size
+		isZero bool
+		out    string
+	}{
+		{Size{}, true, "(0:00x0:00)"},
+		{Size{1, 2}, false, "(0:01x0:02)"},
+		{Size{1 * DIP, 2 * DIP}, false, "(1:00x2:00)"},
+	}
+
+	for i, v := range cases {
+		if out := v.in.IsZero(); out != v.isZero {
+			t.Errorf("Case %d:  Failed predicate IsZero, got %v, want %v", i, out, v.isZero)
+		}
+		if out := v.in.String(); out != v.out {
+			t.Errorf("Case %d:  Failed method String, got %v, want %v", i, out, v.out)
+		}
+	}
 }
