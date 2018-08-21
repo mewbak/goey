f0be44a313cefc67c2e7cb7b58d079b69e01e891
rj <rj@localhost>
2018-08-21 16:18:50 -0400

    Comments in one of the examples to better describe how the package goey should be used.

--- example/onebutton/main.go
+++ example/onebutton/main.go
@@ -25,16 +25,24 @@ func main() {
 }
 
 func createWindow() error {
+	// This is the callback used to initialize the GUI state.  For this simple
+	// example, we need to create a new top-level window, and set a child
+	// widget.
 	mw, err := goey.NewWindow("One Button", render())
 	if err != nil {
 		return err
 	}
+
+	// We store a copy of the pointer to the window so that we can update the
+	// GUI at a later time.
 	mainWindow = mw
 
 	return nil
 }
 
 func updateWindow() {
+	// To update the window, we generate a new widget for the contents of the
+	// top-level window.
 	err := mainWindow.SetChild(render())
 	if err != nil {
 		fmt.Println("Error: ", err.Error())
@@ -42,15 +50,22 @@ func updateWindow() {
 }
 
 func render() base.Widget {
+	// The text for the button will depend on how many times it has been
+	// clicked.  Build the string for the button's caption.
 	text := "Click me!"
 	if clickCount > 0 {
 		text = text + "  (" + strconv.Itoa(clickCount) + ")"
 	}
+
+	// We return a widget describing the desired state of the GUI.  Note that
+	// this is data only, and no changes have been effected yet.
 	return &goey.Padding{
 		Insets: goey.UniformInsets(36 * goey.DIP),
 		Child: &goey.Align{
 			Child: &goey.Button{Text: text, OnClick: func() {
+				// Side-effect for clicking the button.
 				clickCount++
+				// Update the contents of the top-level window.
 				updateWindow()
 			}},
 		},
