2f666baf8d5b1e2872f2e60e1e9504be15ae3208
rj <rj@localhost>
2018-08-30 14:35:48 -0400

    New tests for Run to check error path and edge cases.

--- loop_test.go
+++ loop_test.go
@@ -56,6 +56,13 @@ func ExampleDo() {
 
 func TestRun(t *testing.T) {
 	init := func() error {
+		// Verify that the test is starting in the correct state.
+		if c := atomic.LoadInt32(&mainWindowCount); c != 0 {
+			t.Errorf("Want mainWindow==0, got mainWindow==%d", c)
+			return nil
+		}
+
+		// Create window and verify.
 		window, err := NewWindow("Test", nil)
 		if err != nil {
 			t.Fatalf("Fail in call to NewWindow, %s", err)
@@ -112,15 +119,91 @@ func TestRunWithError(t *testing.T) {
 	} else if s := err.Error(); errorString != s {
 		t.Errorf("Unexpected error, want %s, got %s", errorString, s)
 	}
+	if c := atomic.LoadInt32(&mainWindowCount); c != 0 {
+		t.Errorf("Want mainWindow==0, got mainWindow==%d", c)
+	}
+}
+
+func TestRunWithPanic(t *testing.T) {
+	const errorString = "No luck"
+	defer func() {
+		r := recover()
+		if r != nil {
+			if s, ok := r.(string); !ok {
+				t.Errorf("Unexpected recover, %v", r)
+			} else if s != errorString {
+				t.Errorf("Unexpected recover, %s", s)
+			}
+		} else {
+			t.Errorf("Missing panic")
+		}
+
+		// Make sure that window count is properly maintained.
+		if c := atomic.LoadInt32(&mainWindowCount); c != 0 {
+			t.Errorf("Want mainWindow==0, got mainWindow==%d", c)
+		}
+	}()
+
+	// Make sure that error is passed through to caller
+	init := func() error {
+		panic(errorString)
+	}
+
+	err := Run(init)
+	if err == nil {
+		t.Errorf("Unexpected success, no error returned")
+	} else if s := err.Error(); errorString != s {
+		t.Errorf("Unexpected error, want %s, got %s", errorString, s)
+	}
+	if c := atomic.LoadInt32(&mainWindowCount); c != 0 {
+		t.Errorf("Want mainWindow==0, got mainWindow==%d", c)
+	}
+}
+
+func TestRunWithWindowClose(t *testing.T) {
+	// Make sure that error is passed through to caller
+	init := func() error {
+		if c := atomic.LoadInt32(&mainWindowCount); c != 0 {
+			t.Errorf("Want mainWindow==0, got mainWindow==%d", c)
+			return nil
+		}
+
+		window, err := NewWindow("Test", nil)
+		if err != nil {
+			t.Fatalf("Fail in call to NewWindow, %s", err)
+		}
+		if window == nil {
+			t.Fatalf("Unexpected nil for window")
+		}
+		if c := atomic.LoadInt32(&mainWindowCount); c != 1 {
+			t.Fatalf("Want mainWindow==1, got mainWindow==%d", c)
+		}
+
+		window.Close()
+		return nil
+	}
+
+	err := Run(init)
+	if err != nil {
+		t.Errorf("Unexpected error in call to Run")
+	}
+	if c := atomic.LoadInt32(&mainWindowCount); c != 0 {
+		t.Errorf("Want mainWindow==0, got mainWindow==%d", c)
+	}
 }
 
 func TestDo(t *testing.T) {
 	count := uint32(0)
 
 	init := func() error {
+		// Verify that the test is starting in the correct state.
 		if c := atomic.LoadInt32(&mainWindowCount); c != 0 {
-			t.Fatalf("Want mainWindow==0, got mainWindow==%d", c)
+			t.Errorf("Want mainWindow==0, got mainWindow==%d", c)
+			return nil
 		}
+
+		// Create window and verify.
+		// We need at least one window open to maintain GUI loop.
 		window, err := NewWindow("TestDo", nil)
 		if err != nil {
 			t.Errorf("Failed to create window, %s", err)
