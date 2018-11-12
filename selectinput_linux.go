1732ced49f4024fe90de3e9c1757ad144b5dca7b
rj <rj@localhost>
2018-11-12 16:30:49 -0500

    Added Props for SelectInput widget on windows.

--- selectinput_linux.go
+++ selectinput_linux.go
@@ -60,6 +60,24 @@ func (w *selectinputElement) comboboxtext() *gtk.ComboBoxText {
 	return (*gtk.ComboBoxText)(unsafe.Pointer(w.handle))
 }
 
+func (w *selectinputElement) Props() base.Widget {
+
+	value := w.comboboxtext().GetActive()
+	unset := value<0
+
+	return &SelectInput  {
+		Items   : nil,
+		Value : int(value),
+		Unset    : unset,
+		Disabled: !button.GetSensitive(),
+		OnChange: w.onChange,
+		OnFocus  :w.onFocus.callback,
+		OnBlur  : w.onBlur.callback,
+	}
+	
+	
+}
+
 func (w *selectinputElement) updateProps(data *SelectInput) error {
 	cbt := w.comboboxtext()
 
